0e1c99a6c509706217053f4da9b141923a38e7cb
Horidas Roy <royhoridas64@gmail.com>
2024-08-21 15:28:32 +0600

    alert dialog are added

--- Alert Dialog/app/src/main/java/com/horidas/alertdialog/MainActivity.java
+++ Alert Dialog/app/src/main/java/com/horidas/alertdialog/MainActivity.java
@@ -1,12 +1,16 @@
 package com.horidas.alertdialog;
 
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //single button display box
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        alertDialog.setTitle("Terms & Conditions");
+        alertDialog.setIcon(R.drawable.baseline_info_24);
+        alertDialog.setMessage("Have you read the T & C?");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE,"Yes, you can proceed now",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Proceed", Toast.LENGTH_SHORT).show();
+            }
         });
+        alertDialog.show();
+
+        // double button display box
+
+        AlertDialog.Builder delDialog = new AlertDialog.Builder(MainActivity.this);
+        delDialog.setTitle("Delete");
+        delDialog.setIcon(R.drawable.delete_24);
+        delDialog.setMessage("Are you sure want to delete?");
+        delDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //for yes
+                Toast.makeText(MainActivity.this, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        delDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //for no
+                Toast.makeText(MainActivity.this, "Not Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+        delDialog.show();
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder exitDialog = new AlertDialog.Builder(this);
+        exitDialog.setTitle("Exit");
+        exitDialog.setIcon(R.drawable.exit_to_app_24);
+        exitDialog.setMessage("Are you sure want to exit?");
+        exitDialog.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Welcome back!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        exitDialog.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.super.onBackPressed();
+            }
+        });
+
+        exitDialog.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Operation canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+        exitDialog.show();
     }
 }
