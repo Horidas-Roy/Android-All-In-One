62229770b23b3a6414ca1cf6edfc5418d1019152
Horidas Roy <royhoridas64@gmail.com>
2024-08-23 21:49:47 +0600

    msge,email,call and share activity added

--- ImplicitIntent/app/src/main/java/com/horidas/implicitintent/MainActivity.java
+++ ImplicitIntent/app/src/main/java/com/horidas/implicitintent/MainActivity.java
@@ -1,6 +1,10 @@
 package com.horidas.implicitintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,64 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnDial, btnMsg, btnEmail, btnShare;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnDial = findViewById(R.id.btnDial);
+        btnMsg = findViewById(R.id.btnMsg);
+        btnEmail = findViewById(R.id.btnEmail);
+        btnShare = findViewById(R.id.btnShare);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   /** Intent iDial = new Intent(Intent.ACTION_DIAL);
+                        iDial.setAction(Intent.ACTION_DIAL); */
+
+                Intent iDial = new Intent(Intent.ACTION_DIAL);
+                iDial.setData(Uri.parse("tel: +880 1758734050"));
+                startActivity(iDial);
+            }
         });
+
+        btnMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iMsg = new Intent(Intent.ACTION_SENDTO);
+                iMsg.setData(Uri.parse("smsto:"+Uri.encode("+880 1758734050")));
+                iMsg.putExtra("sms_body","Write message here");
+                startActivity(iMsg);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iEmail = new Intent(Intent.ACTION_SEND);
+                iEmail.setType("message/rfc822");
+                iEmail.putExtra(Intent.EXTRA_EMAIL,new String[]{"royhoridas64@gmail.com","royhoridas65@gmail.com"});
+                iEmail.putExtra(Intent.EXTRA_SUBJECT,"Queries");
+                iEmail.putExtra(Intent.EXTRA_TEXT,"Please resolve this issue");
+                startActivity(Intent.createChooser(iEmail,"Email via"));
+            }
+        });
+
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iShare = new Intent(Intent.ACTION_SEND);
+                iShare.setType("text/plain");
+                iShare.putExtra(Intent.EXTRA_TEXT,"Download this Amazing app https://hajgjg.com");
+                startActivity(Intent.createChooser(iShare,"Share via"));
+            }
+        });
+
+
     }
 }
